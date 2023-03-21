c1d7369a8ffbbc719f61eb1ecfed2f6a08297078
justindegaetano <105392908+justindegaetano@users.noreply.github.com>
2023-03-20 20:19:33 -0400

    Atatus integration

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+atatus.config('1135dfade7a14c8aab0d8a52d8aa3103').install();
 
 const root = document.getElementById('root');
 // const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -23,3 +25,5 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+atatus.notify(new Error('Test Atatus Setup'));
